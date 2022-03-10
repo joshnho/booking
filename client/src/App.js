c4b37322c3aa79a8a1d7fcf5dd7cd42c71df1845
Joshua Nho <joshnho@gmail.com>
2022-03-10 00:54:50 -0500

    initial setup for router + redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-function App() {
+import Home from './components/Pages/Home';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Navbar from './components/Navbar';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Home exact path="/" component={Home} />
+        <Login path="/login" component={Login} />
+        <Register path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Pages/Home.js
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+
+const Home = () => {
+  const state = useSelector((state) => state);
+  return (
+    <div className="container-fluid h1 p-5 text-center">
+      Home Page {JSON.stringify(state)}
+    </div>
+  );
+};
+
+export default Home;
