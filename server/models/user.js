4bef9993d6b69c34be3988a4267455c2ac183242
Joshua Nho <joshnho@gmail.com>
2022-03-10 20:23:34 -0500

    hash user pw

--- server/models/user.js
+++ server/models/user.js
@@ -1,28 +1,47 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const { Schema } = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 64,
+    },
+    stripe_account_id: '',
+    stripe_seller: {},
+    stripeSession: {},
   },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 64,
-  },
-  stripe_account_id: '',
-  stripe_seller: {},
-  stripeSession: {},
+  { timestamps: true }
+);
+
+userSchema.pre('save', function (next) {
+  if (user.isModified('password')) {
+    return bcrypt.hash(this.password, 12, function (err, hash) {
+      if (err) {
+        console.log('Password Hash Error', err);
+        return next(err);
+      }
+      user.password = hash;
+      return next();
+    });
+  } else {
+    return next();
+  }
 });
 
 export default mongoose.model('User', userSchema);
