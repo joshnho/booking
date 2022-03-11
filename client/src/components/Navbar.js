64f7f7a7f0b49cafeaf0ff98c983c0c2bbfe53fd
Joshua Nho <joshnho@gmail.com>
2022-03-11 00:29:27 -0500

    added logout functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,39 @@
-import { Link } from 'react-router-dom'
-
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const { auth } = useSelector((state) => ({ ...state }));
+
+  const logout = () => {
+    dispatch({
+      type: 'LOGOUT',
+      payload: null,
+    });
+    window.localStorage.removeItem('auth');
+  };
+
   return (
     <div className="nav bg-light d-flex justify-content-between">
-      <Link className="nav-link" to="/">Home</Link>
-      <Link className="nav-link" to="/login">Login</Link>
-      <Link className="nav-link" to="/register">Register</Link>
-  </div>
-  )
-}
+      <Link className="nav-link" to="/">
+        Home
+      </Link>
+      {auth ? (
+        <Link className="nav-link" onClick={logout} to="/login">
+          Logout
+        </Link>
+      ) : (
+        <>
+          <Link className="nav-link" to="/login">
+            Login
+          </Link>
+          <Link className="nav-link" to="/register">
+            Register
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
